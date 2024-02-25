5c1f0562a0603bf743963efacaf841c56919beb0
JC144 <1168432+JC144@users.noreply.github.com>
2024-02-25 11:02:26 +0100

    Code pour récupérer les jours Zenflex et générer un json ou les afficher dans la console

---
+++ ApiCaller.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace EDF_Zenflex_Exporter
+{
+    public class ApiCaller : IDisposable
+    {
+        private readonly HttpClient _client = new HttpClient();
+
+        public ApiCaller()
+        {
+            _client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; AcmeInc/1.0)");
+            _client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+        }
+
+        public async Task<string> GetDayTypeAsync(string date)
+        {
+            string dayType = String.Empty;
+
+            try
+            {
+                string url = $"https://particulier.edf.fr/services/rest/opm/getOPMStatut?dateRelevant={date}";
+                using HttpResponseMessage response = await _client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string responseString = await response.Content.ReadAsStringAsync();
+#if DEBUG
+                Console.WriteLine($"{date} {responseString}");                
+#endif
+
+                if (!string.IsNullOrEmpty(responseString))
+                {
+                    var responseJson = JObject.Parse(responseString);
+                    dayType = responseJson["couleurJourJ"].Value<string>();
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.InnerException);
+            }
+
+            return dayType;
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using EDF_Zenflex_Exporter;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var startDate = new DateTime(2023, 10, 15);
+            var endDate = new DateTime(2024, 04, 15);
+
+            //DisplayZenFlexDaysForPeriodAsync(startDate, endDate);
+            await CreateJsonForPeriodAsync(startDate, endDate);
+        }
+
+        static async Task DisplayZenFlexDaysForPeriodAsync(DateTime startDate, DateTime endDate)
+        {
+            int zenFlexCount = 0;
+
+            using (ApiCaller api = new ApiCaller())
+            {
+                while (startDate <= endDate)
+                {
+                    var dayType = await api.GetDayTypeAsync(startDate.ToString("yyyy-MM-dd"));
+
+                    if (dayType == "ZENF_PM")
+                    {
+                        Console.WriteLine($"{startDate.ToString("yyyy-MM-dd")},");
+                        zenFlexCount++;
+                    }
+                    startDate = startDate.AddDays(1);
+                }
+            }
+
+            if (zenFlexCount == 20)
+            {
+                Console.WriteLine("Success");
+            }
+            else if (zenFlexCount < 20)
+            {
+                Console.WriteLine($"Missing {20 - zenFlexCount} days.");
+            }
+            else if (zenFlexCount > 20)
+            {
+                Console.WriteLine($"{zenFlexCount} days found.");
+            }
+        }
+
+        static async Task CreateJsonForPeriodAsync(DateTime startDate, DateTime endDate)
+        {
+            var jsonArray = new JArray();
+            string beginYear = startDate.ToString("yyyy");
+
+            using (ApiCaller api = new ApiCaller())
+            {
+                while (startDate <= endDate)
+                {
+                    Console.WriteLine("Processing date: " + startDate.ToString("yyyy-MM-dd"));
+
+                    var dayType = await api.GetDayTypeAsync(startDate.ToString("yyyy-MM-dd"));
+                    if (!String.IsNullOrEmpty(dayType))
+                    {
+                        JObject jsonEntry = new JObject();
+                        jsonEntry["Date"] = startDate.ToString("yyyy-MM-dd");
+                        jsonEntry["CouleurJourJ"] = dayType;
+                        jsonArray.Add(jsonEntry);
+                    }
+                    startDate = startDate.AddDays(1);
+                }
+            }
+
+            string jsonString = jsonArray.ToString(Formatting.Indented);
+            string outputPath = Path.Combine(Directory.GetCurrentDirectory(), $"{beginYear}_ZenflexCalendar.json");
+            File.WriteAllText(outputPath, jsonString);
+            Console.WriteLine("JSON file created at: " + outputPath);
+        }
+    }
+}
